f38190313a2c6c8dbdfdf122ef60ee1f7e53187b
Mattias <mw222rs@student.lnu.se>
2016-02-04 22:00:54 +0100

    devise model generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :api_keys
   root "api_keys#index"
   # The priority is based upon order of creation: first created -> highest priority.
