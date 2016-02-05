2e0a3e4bf7604690b51c5387ed1d67d106fbf6d7
Mattias <mw222rs@student.lnu.se>
2016-02-05 04:50:12 +0100

    removed gravtastic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  include Gravtastic
-  gravtastic
   has_many :api_keys
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
