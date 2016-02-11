42fbaf9db761b7e74882065b4644a55a2abfb5c2
Mattias <mw222rs@student.lnu.se>
2016-02-11 23:20:03 +0100

    messing about with landing page and admin namespace

--- app/controllers/admin/api_keys_controller.rb
+++ app/controllers/admin/api_keys_controller.rb
@@ -1,5 +1,12 @@
 class Admin::ApiKeysController < ApplicationController
+  before_action :authenticate_admin
   def index
     @api_keys = ApiKey.all
+  end
+
+  private
+
+  def authenticate_admin
+    redirect_to unauthenticated_root_path unless user_logged_in && current_user.admin?
   end
 end
