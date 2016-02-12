6b4e4bca90a82b93bffe5ffe65cc8ba34be9e399
Mattias <mw222rs@student.lnu.se>
2016-02-12 10:58:24 +0100

    weird errors

--- app/controllers/admin/api_keys_controller.rb
+++ app/controllers/admin/api_keys_controller.rb
@@ -4,9 +4,19 @@ class Admin::ApiKeysController < ApplicationController
     @api_keys = ApiKey.all
   end
 
+  def new
+
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
   private
 
   def authenticate_admin
-    redirect_to unauthenticated_root_path unless user_logged_in && current_user.admin?
+    redirect_to unauthenticated_root_path unless user_signed_in? && current_user.admin?
   end
 end
