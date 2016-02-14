cedbd76e1a13bee19cbbeabf29f3c2522f28b7c1
Mattias <mw222rs@student.lnu.se>
2016-02-14 17:15:09 +0100

    falsh partial

--- app/controllers/admin/api_keys_controller.rb
+++ app/controllers/admin/api_keys_controller.rb
@@ -1,22 +1,34 @@
 class Admin::ApiKeysController < ApplicationController
   before_action :authenticate_admin
+  before_action :set_api_keys, only: :destroy
+
   def index
     @api_keys = ApiKey.all.search(params[:keyword])
   end
 
-  def new
-
-  end
-
-  def edit
-  end
-
   def destroy
+    @api_key.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_api_keys_url, notice: "API key for \"#{@api_key.name}\" was successfully revoked." }
+      format.json { head :no_content }
+    end
   end
 
   private
 
   def authenticate_admin
     redirect_to unauthenticated_root_path unless user_signed_in? && current_user.admin?
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+
+  def set_api_keys
+    @api_key = ApiKey.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+
+  def api_key_params
+    params.require(:api_key).permit(:name, :key)
   end
 end
