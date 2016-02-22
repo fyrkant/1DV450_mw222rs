de2302376520f0ffa22287da93312f52efa30367
Mattias <mw222rs@student.lnu.se>
2016-02-22 22:57:35 +0100

    should I minitest or specify?

--- Gemfile
+++ Gemfile
@@ -26,6 +26,11 @@ gem "sdoc", "~> 0.4.0", group: :doc
 
 gem "rails_12factor", group: :production
 
+group :test do
+  gem "test-unit", group: :test
+  gem 'shoulda', '~> 3.5'
+  gem 'shoulda-matchers', '~> 2.0'
+end
 # Use ActiveModel has_secure_password
 # gem "bcrypt", "~> 3.1.7"
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,26 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  # Shoulda::Matchers.configure do |config|
+  #   config.integrate do |with|
+  #     # Choose a test framework:
+  #     # with.test_framework :rspec
+  #     with.test_framework :minitest
+  #     # with.test_framework :minitest_4
+  #     # with.test_framework :test_unit
+
+  #     # # Choose a library:
+  #     # with.library :active_record
+  #     # with.library :active_model
+  #     # with.library :action_controller
+  #     # Or, choose all of the above:
+  #     with.library :rails
+  #   end
+  # end
 end
