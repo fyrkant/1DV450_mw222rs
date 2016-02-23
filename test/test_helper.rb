24851e4039bdf26f18bfb95c7db56638233902cb
Mattias <mw222rs@student.lnu.se>
2016-02-23 14:59:44 +0100

    json helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,15 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
+require "minitest/pride"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  def json(body)
+    JSON.parse(body, symbolize_names: true)
+  end
 
   # Add more helper methods to be used by all tests here...
   # Shoulda::Matchers.configure do |config|
