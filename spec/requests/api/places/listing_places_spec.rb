4e6b1bef9d71d6b517e0bdcd9086198484dc4f82
Mattias <mw222rs@student.lnu.se>
2016-03-04 22:55:11 +0100

    starting test for api key validation

--- spec/requests/api/places/listing_places_spec.rb
+++ spec/requests/api/places/listing_places_spec.rb
@@ -1,40 +1,53 @@
 require "rails_helper"
 
 RSpec.describe "ListingPlaces" do
-  let!(:place1) { Place.create!(name: "Some place", lat: 12.34, lng: 34.45) }
-  let!(:place2) { Place.create!(name: "Another place", lat: 23.2233, lng: 23.344) }
-  let!(:event1) { Event.create!(name: "Party!", description: "Lorem ipsum dolores whatever", place: place1, date: rand(300).days.from_now) }
-
-  describe "list all places" do
-    it "lists all places" do
+  context "without vlaid api key" do
+    it "returns 401" do
       get "/api/places"
 
-      expect(response).to have_http_status 200
+      expect(response).to have_http_status 401
       expect(response.content_type).to eq Mime::JSON
-
-      json = json(response.body)
-      expect(json[:data].length).to eq 2
     end
   end
-  describe "get single place" do
-    it "returns existing resource" do
-      get "/api/places/#{place1.id}"
+  context "with valid API key" do
 
-      expect(response).to have_http_status 200
-      expect(response.content_type).to eq Mime::JSON
+    # TODO: Add api key to header.
 
-      json = json(response.body)
-      expect(json[:data][:attributes][:name]).to eq place1.name
+    let!(:place1) { Place.create!(name: "Some place", lat: 12.34, lng: 34.45) }
+    let!(:place2) { Place.create!(name: "Another place", lat: 23.2233, lng: 23.344) }
+    let!(:event1) { Event.create!(name: "Party!", description: "Lorem ipsum dolores whatever", place: place1, date: rand(300).days.from_now) }
+
+    describe "list all places" do
+      it "lists all places" do
+        get "/api/places"
+
+        expect(response).to have_http_status 200
+        expect(response.content_type).to eq Mime::JSON
+
+        json = json(response.body)
+        expect(json[:data].length).to eq 2
+      end
     end
-    it "returns 404 and error object when requesting non-existent resource" do
-      get "/api/places/2023124235042934"
+    describe "get single place" do
+      it "returns existing resource" do
+        get "/api/places/#{place1.id}"
 
-      expect(response).to have_http_status 404
-      expect(response.content_type).to eq Mime::JSON
+        expect(response).to have_http_status 200
+        expect(response.content_type).to eq Mime::JSON
 
-      json = json(response.body)
-      expect(json[:status]).to eq 404
-      expect(json[:detail]).to eq "Resource for '/api/places/2023124235042934' could not be found"
+        json = json(response.body)
+        expect(json[:data][:attributes][:name]).to eq place1.name
+      end
+      it "returns 404 and error object when requesting non-existent resource" do
+        get "/api/places/2023124235042934"
+
+        expect(response).to have_http_status 404
+        expect(response.content_type).to eq Mime::JSON
+
+        json = json(response.body)
+        expect(json[:status]).to eq 404
+        expect(json[:detail]).to eq "Resource for '/api/places/2023124235042934' could not be found"
+      end
     end
   end
 end
